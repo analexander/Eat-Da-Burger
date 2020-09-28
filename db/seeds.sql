2e163e997d6e4b9a813bf85eccc20f81473f9348
Angel Alexander <angale22@gmail.com>
2020-09-28 15:05:24 -0500

    adding seeds.sql with values

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bobs Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lair of the Minotaur', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plague Bringer', true);
