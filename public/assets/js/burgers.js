25e59dac2f2a215d5d5a4290a841c426cf2b4b4d
Angel Alexander <angale22@gmail.com>
2020-10-03 19:59:06 -0500

    trying to get partial to link

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".update-button").on("click", function(event) {
+    $(".devour").on("click", function(event) {
       var id = $(this).data("id");
   
       var newDevState = {
